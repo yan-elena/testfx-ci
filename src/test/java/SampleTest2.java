f9907f734a894c50da5915a25ecc55dfe7d8f396
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 23:07:35 +0100

    test: property

--- src/test/java/SampleTest2.java
+++ src/test/java/SampleTest2.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.util.WaitForAsyncUtils;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ApplicationExtension.class)
 public class SampleTest2 {
 
     static {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-        }
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("java.awt.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        WaitForAsyncUtils.checkAllExceptions = false;
+        WaitForAsyncUtils.autoCheckException = false;
+//        if (Boolean.getBoolean("headless")) {
+//            System.setProperty("testfx.robot", "glass");
+//            System.setProperty("testfx.headless", "true");
+//            System.setProperty("prism.order", "sw");
+//            System.setProperty("prism.text", "t2k");
+//        }
     }
 
     @Test
