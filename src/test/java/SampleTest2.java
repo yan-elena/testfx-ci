89ddc21920a34ba0baedc2c7ccf4e0903da30c04
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 23:15:08 +0100

    test: sample test

--- src/test/java/SampleTest2.java
+++ src/test/java/SampleTest2.java
@@ -1,12 +1,20 @@
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
 import org.testfx.util.WaitForAsyncUtils;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ApplicationExtension.class)
 public class SampleTest2 {
+
+    private Button button;
 
     static {
         System.setProperty("testfx.robot", "glass");
@@ -16,16 +24,47 @@ public class SampleTest2 {
         System.setProperty("prism.text", "t2k");
         WaitForAsyncUtils.checkAllExceptions = false;
         WaitForAsyncUtils.autoCheckException = false;
-//        if (Boolean.getBoolean("headless")) {
-//            System.setProperty("testfx.robot", "glass");
-//            System.setProperty("testfx.headless", "true");
-//            System.setProperty("prism.order", "sw");
-//            System.setProperty("prism.text", "t2k");
-//        }
     }
 
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+    @Start
+    private void start(Stage stage) {
+        button = new Button("click me!");
+        button.setId("myButton");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+        stage.setScene(new Scene(new StackPane(button), 100, 100));
+        stage.show();
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
     @Test
-    public void test() {
-        assertTrue(true);
+    void should_contain_button_with_text(FxRobot robot) {
+        FxAssert.verifyThat(button, LabeledMatchers.hasText("click me!"));
+        // or (lookup by css id):
+        FxAssert.verifyThat("#myButton", LabeledMatchers.hasText("click me!"));
+        // or (lookup by css class):
+        FxAssert.verifyThat(".button", LabeledMatchers.hasText("click me!"));
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    void when_button_is_clicked_text_changes(FxRobot robot) {
+        // when:
+        robot.clickOn(".button");
+
+        // then:
+        FxAssert.verifyThat(button, LabeledMatchers.hasText("clicked!"));
+        // or (lookup by css id):
+        FxAssert.verifyThat("#myButton", LabeledMatchers.hasText("clicked!"));
+        // or (lookup by css class):
+        FxAssert.verifyThat(".button", LabeledMatchers.hasText("clicked!"));
     }
 }
