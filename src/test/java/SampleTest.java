49da31838893b133e139f6f6f8b24f62af3fa147
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 21:46:08 +0100

    test: add javafx test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest extends ApplicationTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
