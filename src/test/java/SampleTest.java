5333ef064e815faa3ce23adcd8f548107b3ae870
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 22:14:16 +0100

    test: extension test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,10 +3,10 @@ import org.testfx.framework.junit5.ApplicationTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SampleTest extends ApplicationTest {
+public class SampleTest { //extends ApplicationTest {
 
     @Test
     public void test() {
-        assertTrue(false);
+        assertTrue(true);
     }
 }
