968494ec1f844cf8d74c8cd22710cee7fda0c7af
Elena Yan <elena.yan@studio.unibo.it>
2023-01-30 21:39:46 +0100

    build: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     java
+    application
+    id("com.github.johnrengelman.shadow") version "5.2.0"
     alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.javafx)
 }
 
 group = "org.example"
@@ -10,9 +13,30 @@ repositories {
     mavenCentral()
 }
 
+// List of JavaFX modules you need. Comment out things you are not using.
+val javaFXModules = listOf(
+        "base",
+        "controls",
+        "fxml",
+        "swing",
+        "graphics"
+)
+// All required for OOP
+val supportedPlatforms = listOf("linux", "mac", "win")
+
 dependencies {
-    testImplementation(rootProject.libs.junit.api)
-    testRuntimeOnly(rootProject.libs.junit.engine)
+    implementation(libs.bundles.vertx.dependencies)
+    implementation(libs.gson)
+    // JavaFX: comment out if you do not need them
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:19:$platform")
+        }
+    }
+    //testImplementation(libs.awaitility)
+    testImplementation(libs.bundles.testfx)
+    testImplementation(libs.junit.api)
+    testRuntimeOnly(libs.junit.engine)
 }
 
 tasks.getByName<Test>("test") {
